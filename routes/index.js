67957e57d6fccdcbd2483e6c6eeedf1e6c32cb96
braile <braile.c.gawigawen@gmail.com>
2024-08-05 21:43:03 +0800

    [ROUTE] remove console in / route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  console.log({ log: "log from exp" });
   res.json({ log: "hello from expressjs!" });
 });
 
